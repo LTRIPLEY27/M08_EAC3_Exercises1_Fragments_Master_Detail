f54e3037e450cc7ce465d31c5075d2c8d3a06be8
isaDevs <isabel.calzadilla.18@gmail.com>
2022-11-11 12:02:12 +0100

    add new funtionalitys

--- app/src/main/java/isabelcalzadilla/ioc/fragmentsactivity/FirtsFragment.java
+++ app/src/main/java/isabelcalzadilla/ioc/fragmentsactivity/FirtsFragment.java
@@ -1,5 +1,6 @@
 package isabelcalzadilla.ioc.fragmentsactivity;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -18,6 +19,32 @@ public class FirtsFragment extends Fragment {
     private final int YES = 0;
     private final int NO = 1;
 
+    // constante para añadir una interfaz con llamada
+    private static final int NONE = 2;
+    private static final String CHOICE = "choice";
+
+    //RADIO BUTTONS VARIABLES
+    private int radioChoice = NONE;
+
+    // definicion de la variable del listener
+    OnFragmentInteractionListener listen;
+
+    // CREACION DE INTERFACE
+    interface  OnFragmentInteractionListener {
+        void radiusChoice(int choice);
+    }
+
+    // MÉTODO 'ONATTACH'
+    // la implementación del método se da en la ACTIVITY
+    @Override
+    public void onAttach(Context context){
+        super.onAttach(context);
+        if(context instanceof OnFragmentInteractionListener){
+            listen = (OnFragmentInteractionListener) context;
+        } else {
+            throw new ClassCastException(context.toString() /*+ getResources().getString((R.string.exception_message)*/);
+        }
+    }
 
     // CONSTRUCTOR VACÍO PARA INSTANCIAR FRAGMENS (IMPERATIVO)
     public FirtsFragment() {
@@ -33,6 +60,13 @@ public class FirtsFragment extends Fragment {
 
         final RadioGroup radios = rootie.findViewById(R.id.radios);
 
+        // ADICIÓN DE LA EVALUACIÓN DEL BUNDLE
+        if(getArguments().containsKey(CHOICE)) {
+            radioChoice = getArguments().getInt(CHOICE);
+        } if(radioChoice != NONE) {
+            radios.check(radios.getChildAt(radioChoice).getId());
+        }
+
         // SETTEO DE LOS VALORES DEL RADIOGROUP
         radios.setOnCheckedChangeListener(
                 new RadioGroup.OnCheckedChangeListener() {
@@ -46,14 +80,22 @@ public class FirtsFragment extends Fragment {
 
                         TextView texti = rootie.findViewById(R.id.fragment_header);
 
+                        // ADICIÓN DEL RADIO BUTTON
                         switch (indice) {
                             case YES:
                                 texti.setText(R.string.yes_message);
+                                // LLAMA AL EVENTO DEL RADIOBUTTON CON EL VALOR INT DE YES O NO
+                                radioChoice = YES;
+                                listen.radiusChoice(YES);
                                 break;
                             case NO:
                                 texti.setText(R.string.no_message);
+                                radioChoice = NO;
+                                listen.radiusChoice(NO);
                                 break;
                             default:
+                                radioChoice = NO;
+                                listen.radiusChoice(NO);
                                 break;
                         }
                     }
@@ -63,7 +105,13 @@ public class FirtsFragment extends Fragment {
 
     // CÓDIGO PARA ADHERIR MULTIPLES FRAGMENTS, SE DEBE DE NECESARIAMENTE CREAR NUEVAS INSTANCIAS
 
-    public static FirtsFragment neoFragment(){
-        return new FirtsFragment();
+    public static FirtsFragment neoFragment(int choice){
+        FirtsFragment fragment = new FirtsFragment();
+        Bundle argumentos = new Bundle();
+        argumentos.putInt(CHOICE, choice);
+
+        fragment.setArguments(argumentos);
+        //return new FirtsFragment(); --> old
+        return fragment;
     }
 }
--- app/src/main/java/isabelcalzadilla/ioc/fragmentsactivity/MainActivity.java
+++ app/src/main/java/isabelcalzadilla/ioc/fragmentsactivity/MainActivity.java
@@ -1,16 +1,22 @@
 package isabelcalzadilla.ioc.fragmentsactivity;
 
+import android.content.Context;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+// implementamos la INTERFACE DECLARADA COMO LISTENER PARA RECIBIR LOS DATOS QUE SE GENEREN ALLÍ
+public class MainActivity extends AppCompatActivity implements FirtsFragment.OnFragmentInteractionListener{
 
     private Button show;
     private boolean isShower = false;
+
+    // declaracion de una variable para el radio y el manejo de la interface
+    private int radio = 2; // --> 2 == NO BY DEFAULT
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,7 +35,8 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void displayFragment(){
-        FirtsFragment createdInstance = FirtsFragment.neoFragment();
+        // PASO DEL VALOR DEL BUTTON A LA INSTANCIA DEL FRAGMENR
+        FirtsFragment createdInstance = FirtsFragment.neoFragment(radio);
 
         // FRAGMENT MANAGER
         FragmentManager manager = getSupportFragmentManager(); //método defecto
@@ -41,4 +48,10 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    // MÉTODO DEL FRAGMENT
+    @Override
+    public void radiusChoice(int choice) {
+        radio = choice;
+        Toast.makeText(this, "ELECCION " + Integer.toString(choice), Toast.LENGTH_SHORT).show();
+    }
 }
